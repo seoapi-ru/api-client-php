4040b2ea53e1f025a947085babdb5d961717032f
nkonev <velosipedist.org@gmail.com>
2019-06-17 16:35:26 +0300

    Add Faker

--- tests/Unit/UnitTestCase.php
+++ tests/Unit/UnitTestCase.php
@@ -2,9 +2,17 @@
 
 namespace Tests\Unit;
 
+use Faker\Factory;
 use PHPUnit\Framework\TestCase;
 
 class UnitTestCase extends TestCase
 {
+    /** @var \Faker\Generator */
+    protected $faker;
 
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->faker = Factory::create('ru');
+    }
 }
